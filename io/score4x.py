dfc37e00b5a103dc8c1d19587a4a468fd13eee77
Simon Gibbs <s.gibbs@samsung.com>
2014-12-24 14:45:34 -0800

    Adding stub score function

---
+++ io/score4x.py
@@ -0,0 +1,12 @@
+##
+# @brief Score a 4x data array.
+#
+# @param darray A 4x data array.
+#
+# @author
+#
+# Iterates over a 4x data array and adds a score element for each entry:
+#  0 - the prices went down in the last 10 minutes
+#  1 - the price went up or stayed the same
+def score_4xdata(darray):
+    print("scoring in progress");
--- io/test.py
+++ io/test.py
@@ -1,7 +1,10 @@
 from read4x import read_4xfile
+from score4x import score_4xdata
 
-ok = read_4xfile("abc", [])
+darray = []
+ok = read_4xfile("abc", darray)
 if ok:
 	print("read_4xfile passed")
+	score_4xdata(darray)
 else:
 	print("read_4xfile failed")
