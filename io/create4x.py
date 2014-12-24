65d24fa1e7f62f6dca63bb659dae95c906bb982a
Simon Gibbs <s.gibbs@samsung.com>
2014-12-24 15:59:17 -0800

    Adding a function to create test data

---
+++ io/create4x.py
@@ -0,0 +1,18 @@
+##
+# @brief Create a 4x data array.
+#
+#
+# @author s5g
+#
+def create_4xdata():
+	# tstamp, open, high, low, close score
+    darray = [
+    	[0, 1, 2, 1, 1, None],
+    	[2, 1, 2, 1, 1, None],
+    	[4, 1, 2, 1, 1, None],
+    	[6, 1, 2, 1, 1, None],
+    	[8, 1, 2, 1, 1, None],
+    	[10, 1, 2, 1, 2, None],
+    	[12, 2, 2, 2, 3, None],
+    ]
+    return darray
--- io/test.py
+++ io/test.py
@@ -1,10 +1,21 @@
 from read4x import read_4xfile
 from score4x import score_4xdata
+from create4x import create_4xdata
 
-darray = []
-ok = read_4xfile("abc", darray)
-if ok:
-	print("read_4xfile passed")
+test_create = True;
+test_read = False;
+
+if test_create:
+	print("Running create4x test")
+	darray = create_4xdata()
 	score_4xdata(darray)
-else:
-	print("read_4xfile failed")
+
+if test_read:
+	print("Running read4x test")
+	darray = []
+	ok = read_4xfile("abc", darray)
+	if ok:
+		print("read_4xfile passed")
+		score_4xdata(darray)
+	else:
+		print("read_4xfile failed")
