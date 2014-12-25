5ceafe43d6dd2daa05f11bc19e60e7f54bb52cf2
itsnotafake <itsnotafake@gmail.com>
2014-12-24 16:24:05 -0800

    implemented the score4x function

--- io/score4x.py
+++ io/score4x.py
@@ -6,7 +6,17 @@
 # @author
 #
 # Iterates over a 4x data array and adds a score element for each entry:
-#  0 - the prices went down in the last 10 minutes
-#  1 - the price went up or stayed the same
+#  0 - the closing price went down at the end of 10 minutes
+#  1 - the closing price went up or stayed the same at the end of 10 minutes
 def score_4xdata(darray):
-    print("scoring in progress");
+	data_array_length = len(darray)
+	print (data_array_length)
+	for x in range (0, data_array_length - 5):
+		row1 = darray[x]
+		row2 = darray[x+5]
+		if(row2[4] < row1[4]):
+			row1[5] = 0
+		else:
+			row1[5] = 1 
+		print ("x= " + str(x) + ", close1 = " + str(row1[4]) + ", close2 = " + str(row2[4]) + ", score1 = " + str(row1[5]))
+
